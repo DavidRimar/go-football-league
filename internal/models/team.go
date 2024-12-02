cc068c8307d13c0c789be86bb8cb0f90e03a3155
David Rimar <rimardavid@gmail.com>
2024-12-02 21:15:21 +0100

    add more fields to teams

--- internal/models/team.go
+++ internal/models/team.go
@@ -1,6 +1,8 @@
 package models
 
 type Team struct {
-	ID   string `json:"id" bson:"_id"`
-	Name string `json:"name" bson:"name"`
+	ID              string `json:"id" bson:"_id"`
+	Name            string `json:"name" bson:"name"`
+	Stadium         string `json:"stadium" bson:"stadium"`
+	StadiumCapacity int64  `json:"stadiumCapacity" bson:"stadiumCapacity"`
 }
